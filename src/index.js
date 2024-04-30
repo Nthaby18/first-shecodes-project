e4b4f2e1b76e564ca169f93285b34e8f6b62ee57
Nthaby18 <mahaoshorde9881@gmail.com>
2024-04-30 14:26:21 +0200

    made changes index html,index.js ,index style.css

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-cityElement.innerHTML = response.data.city;
-timeElement.innerHTML = formatDate(date);
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+}
 
 function formatDate(date) {
-  return"Tuesday" 18;48;
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let days = [
